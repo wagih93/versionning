5f18d1b89b792a68198f7748bf389af55fefa67d
wagih <wagih.belg@gmail.com>
2021-12-24 22:48:37 +0100

    base app container

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import EmployeePage from './components/EmployeePage'
+import HomePage from './components/HomePage'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(){
+    super()
+    this.state={
+      clickedEmployee : null,
+      searchedText : '',
+      employees : [
+        {
+          id : 1,
+          name : 'Anis',
+          poste : 'CEO',
+          phone : '22323424',
+          email : 'anis@itbs.tn',
+          img : 'avatarM1.png'
+        },
+        {
+          id : 2,
+          name : 'Aymen',
+          poste : 'CTO',
+          phone : '53525323',
+          email : 'aymen@itbs.tn',
+          img : 'avatarM1.png'
+        },
+        {
+          id : 3,
+          name : 'Ahmed',
+          poste : 'CMO',
+          phone : '98383484',
+          email : 'ahmed@itbs.tn',
+          img : 'avatarM1.png'
+        }
+      ]
+    }
+  }
+
+  clickedItem = (id=null)=>{
+    this.setState({
+      ...this.state,
+      clickedEmployee : id
+    })
+  }
+
+  handleSearch = (searchedText) => {
+    this.setState({
+      ...this.state,
+      searchedText
+    },()=>{console.log(this.state.searchedText)} )
+  }
+
+  render() {
+    const filtredEmployees = this.state.employees.filter((employee)=>(
+      employee.name.includes(this.state.searchedText) ||
+      employee.poste.includes(this.state.searchedText) ||
+      employee.phone.includes(this.state.searchedText) || 
+      employee.email.includes(this.state.searchedText)
+    ))
+    return (
+        <div className="App"  style={{display : 'flex'}}>
+          <HomePage 
+            employees={!this.state.searchedText ? this.state.employees : filtredEmployees} 
+            clickedItem={this.clickedItem} 
+            handleSearch={this.handleSearch}
+            clickedEmployee={this.state.clickedEmployee}
+          />
+          <EmployeePage 
+            employee={this.state.employees.find((employee)=>  (employee.id === this.state.clickedEmployee ))} 
+            clickedItem={this.clickedItem}
+          />
+          
+          
+          
+        </div>
+    )
+  }
 }
 
-export default App;
+export default App
