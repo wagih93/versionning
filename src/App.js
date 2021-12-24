91f1a3d61cf966fce7732def638d0c1791c5286d
wagih <wagih.belg@gmail.com>
2021-12-25 00:15:26 +0100

    Router & json server

--- src/App.js
+++ src/App.js
@@ -3,80 +3,46 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import EmployeePage from './components/EmployeePage'
 import HomePage from './components/HomePage'
+import { Card } from 'react-bootstrap';
+import {Route,  BrowserRouter as Router, Switch} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './js/store'
 
 
 class App extends Component {
   constructor(){
     super()
     this.state={
-      clickedEmployee : null,
-      searchedText : '',
-      employees : [
-        {
-          id : 1,
-          name : 'Anis',
-          poste : 'CEO',
-          phone : '22323424',
-          email : 'anis@itbs.tn',
-          img : 'avatarM1.png'
-        },
-        {
-          id : 2,
-          name : 'Aymen',
-          poste : 'CTO',
-          phone : '53525323',
-          email : 'aymen@itbs.tn',
-          img : 'avatarM1.png'
-        },
-        {
-          id : 3,
-          name : 'Ahmed',
-          poste : 'CMO',
-          phone : '98383484',
-          email : 'ahmed@itbs.tn',
-          img : 'avatarM1.png'
-        }
-      ]
+      searchedText : ''
     }
   }
 
-  clickedItem = (id=null)=>{
-    this.setState({
-      ...this.state,
-      clickedEmployee : id
-    })
-  }
-
+  
   handleSearch = (searchedText) => {
     this.setState({
       ...this.state,
       searchedText
-    },()=>{console.log(this.state.searchedText)} )
+    })
   }
 
   render() {
-    const filtredEmployees = this.state.employees.filter((employee)=>(
-      employee.name.includes(this.state.searchedText) ||
-      employee.poste.includes(this.state.searchedText) ||
-      employee.phone.includes(this.state.searchedText) || 
-      employee.email.includes(this.state.searchedText)
-    ))
+    
     return (
-        <div className="App"  style={{display : 'flex'}}>
-          <HomePage 
-            employees={!this.state.searchedText ? this.state.employees : filtredEmployees} 
-            clickedItem={this.clickedItem} 
-            handleSearch={this.handleSearch}
-            clickedEmployee={this.state.clickedEmployee}
-          />
-          <EmployeePage 
-            employee={this.state.employees.find((employee)=>  (employee.id === this.state.clickedEmployee ))} 
-            clickedItem={this.clickedItem}
-          />
-          
-          
-          
-        </div>
+      <Provider store={store}>
+        <Router>
+          <Card className="App borderless">
+            <Switch>
+              <Route  path="/employee/:idEmployee">
+                <EmployeePage />
+              </Route>
+              <Route exact path="/">
+                <HomePage handleSearch={this.handleSearch} />
+              </Route>
+            </Switch>
+          </Card>
+        </Router>
+      </Provider>
+     
     )
   }
 }
